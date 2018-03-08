4a9606c98a5d1fb77b7039b2ed101857120b472c
bran <liaminscnu@outlook.com>
2018-03-08 20:39:05 +0800

    增加API中关于注册的验证细节

--- js/options.js
+++ js/options.js
@@ -27,9 +27,9 @@ $(document).ready(function() {
 	$('#delete-inte-modal').modal();
 
 	// 本机测试用服务器
-	var server_url = 'http://127.0.0.1/BuzzwordReader/';
+	// var server_url = 'http://127.0.0.1/BuzzwordReader/';
 	// 生产环境公网服务器
-	// var server_url = 'http://119.29.58.165:81/index.php/';
+	var server_url = 'http://119.29.58.165:81/index.php/';
 
 
 	// 显示登录用户
@@ -71,7 +71,6 @@ $(document).ready(function() {
 		}else {
 			gender = 1;
 		}
-		alert(username+','+password+','+email+','+phone+','+gender+','+','+profile);
 
 		// ajax 传输
 		$.ajax({
